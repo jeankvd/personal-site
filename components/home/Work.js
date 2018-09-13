d8669d39aa2d22b8840dae276b447e2e9823dbd5
Jean <djjean85@gmail.com>
2018-09-12 21:22:56 -0400

    changing state based on button pressed

--- components/home/Work.js
+++ components/home/Work.js
@@ -10,11 +10,23 @@ class Work extends Component {
     };
   }
 
+  handleClick = (e) => {
+    e.stopPropagation();
+    e.nativeEvent.stopImmediatePropagation();
+    this.setState({
+      selected: e.target.id
+    })
+  }
+
   render() {
     return (
       <div className="work">
         <h1>My Work</h1>
         <p className="lead">To see the code at github click here</p>
+        <ul>
+          <li><span id="showcase" onClick={this.handleClick}>Showcase</span></li>
+          <li><span id="three" onClick={this.handleClick}>321</span></li>
+        </ul>
         <Showcase />
       </div>
     );
